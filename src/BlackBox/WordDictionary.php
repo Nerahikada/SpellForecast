4533a631cef5dd93ef5b9c25fcc0e0e376459966
Nerahikada <hkshirorom@yahoo.co.jp>
2022-11-07 23:29:00 +0900

    単語リストをキャッシュするように

--- src/BlackBox/WordDictionary.php
+++ src/BlackBox/WordDictionary.php
@@ -9,11 +9,29 @@ use Nerahikada\SpellForecast\Word;
 final class WordDictionary
 {
     private const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
-    private const BLACK_LISTS = ['FUCK', 'NIGGER', 'SEX',];
+    private const BLACK_LIST = ['FUCK', 'NIGGER', 'SEX',];
 
     private readonly array $words;
 
     public function __construct()
+    {
+        if (!file_exists('.cache')) {
+            mkdir('.cache');
+        }
+        if (!file_exists('.cache/dictionary.txt')) {
+            $this->downloadFromInternet();
+        }
+
+        $words = [];
+        $handle = fopen('.cache/dictionary.txt', 'r');
+        while(($line = fgets($handle)) !== false){
+            $word = trim($line);
+            $words[sha1($word, true)] = $word;
+        }
+        $this->words = $words;
+    }
+
+    private function downloadFromInternet(): void
     {
         $contents = json_decode(
             file_get_contents(
@@ -24,17 +42,17 @@ final class WordDictionary
         );
 
         $words = [];
+
         foreach ($contents as $content) {
-            $raw = file_get_contents($content['download_url']);
-            foreach (explode("\n", $raw) as $line) {
+            foreach (explode("\n", file_get_contents($content['download_url'])) as $line) {
                 $line = strtoupper(trim($line));
-                if ($line !== '' && ctype_alpha($line) && !in_array($line, self::BLACK_LISTS, true)) {
-                    $words[sha1($line, true)] = $line;
+                if ($line !== '' && ctype_alpha($line) && !in_array($line, self::BLACK_LIST, true)) {
+                    $words[] = $line;
                 }
             }
         }
 
-        $this->words = $words;
+        file_put_contents('.cache/dictionary.txt', implode("\n", $words));
     }
 
     public function contain(Word $word): bool
