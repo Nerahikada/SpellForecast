25a449cc8afd551c3400dc578d3159e80181b180
Nerahikada <hkshirorom@yahoo.co.jp>
2022-11-07 16:30:59 +0900

    ネスト地獄からの解放っ…！！！
    参考: https://stackoverflow.com/a/31113535
    PHPは素晴らしい

--- Main.php
+++ Main.php
@@ -24,75 +24,28 @@ for ($y = 0; $y < $board->size; ++$y) {
     echo PHP_EOL;
 }
 
-/**
- * @yield Path
- */
-function generateAroundPath(Board $board, Path $root) : Generator{
-    foreach($root->latest()->around($board->size) as $position){
-        if(!$root->has($position)) yield $root->append($position);
-    }
-}
-$TOTAL_COUNT = 0;
-function doSomething(Path $path){
-    global $TOTAL_COUNT;
-    $TOTAL_COUNT++;
-}
-for($y = 0; $y < $board->size; ++$y){
-    for($x = 0; $x < $board->size; ++$x){
-        foreach(generateAroundPath($board, new Path(new Position($x, $y))) as $path2){
-            doSomething($path2);
-            foreach(generateAroundPath($board, $path2) as $path3){
-                doSomething($path3);
-                foreach(generateAroundPath($board, $path3) as $path4){
-                    doSomething($path4);
-                    foreach(generateAroundPath($board, $path4) as $path5){
-                        doSomething($path5);
-                        foreach(generateAroundPath($board, $path5) as $path6){
-                            doSomething($path6);
-                            foreach (generateAroundPath($board, $path6) as $path7){
-                                doSomething($path7);
-                                foreach(generateAroundPath($board,$path7) as $path8){
-                                    doSomething($path8);
-                                    foreach(generateAroundPath($board, $path8) as $path9){
-                                        doSomething($path9);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+/** @yield Path */
+function generatePath(Board $board, Path $root, int $depth = 1, int &$current = 0): Generator
+{
+    foreach($root->latest()->around($board->size - 1) as $position){
+        if(!$root->has($position)){
+            $path = $root->append($position);
+            if(++$current < $depth){
+                yield from generatePath($board, $path, $depth, $current);
             }
+            --$current;
+            yield $path;
         }
     }
 }
-echo("All done, counts: ");var_dump($TOTAL_COUNT);
 
-
-exit;
-$findAroundPaths = function (array $roots) use ($board): array {
-    $paths = [];
-    /** @var Path[] $roots */
-    foreach ($roots as $root) {
-        foreach ($root->current()->around($board->size) as $position) {
-            try {
-                $paths[] = $root->append($position);
-            } catch (InvalidArgumentException) {
-            }
-        }
-    }
-    return $paths;
-};
-$pathsArray = [[new Path(new Position(2, 2))]];
-for($length = 2; $length <= 7; ++$length){
-    $pathsArray[$length - 1] = $findAroundPaths($pathsArray[$length - 2]);
+$pathCounts = [];
+foreach(generatePath($board, new Path(new Position(2, 2)), 9) as $path){
+    $c = $path->count();
+    if(!isset($pathCounts[$c])) $pathCounts[$c] = 0;
+    ++$pathCounts[$c];
 }
-foreach($pathsArray as $length => $paths){
-    echo ($length + 1) . " Letters: " . count($paths) . " Paths\n";
-    /*
-    foreach($paths as $key => $path){
-        echo "Path #$key: ";
-        foreach($path as $position) echo "($position->x,$position->y) ";
-        echo "| " . count($path) . " nodes\n";
-    }
-    */
+echo "Start from (2, 2):\n";
+foreach($pathCounts as $length => $count){
+    echo str_pad((string) $length, 2, pad_type: STR_PAD_LEFT) . " Letters: $count Paths\n";
 }
