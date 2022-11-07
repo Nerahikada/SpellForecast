d2dd217ee046a8b97a1f9a2f77c099ccf4d3cd57
Nerahikada <hkshirorom@yahoo.co.jp>
2022-11-07 19:49:59 +0900

    crc32() だと簡単に衝突してしまうため変更

--- src/BlackBox/WordJudge.php
+++ src/BlackBox/WordJudge.php
@@ -6,31 +6,39 @@ namespace Nerahikada\SpellForecast\BlackBox;
 
 use Nerahikada\SpellForecast\Word;
 
-final class WordJudger
+final class WordJudge
 {
+    private const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
     private const BLACK_LISTS = ['FUCK', 'NIGGER', 'SEX',];
+
     private readonly array $words;
 
     public function __construct()
     {
         $contents = json_decode(
-            file_get_contents('https://api.github.com/repos/jacksonrayhamilton/wordlist-english/contents/sources'),
+            file_get_contents(
+                'https://api.github.com/repos/jacksonrayhamilton/wordlist-english/contents/sources',
+                context: stream_context_create(['http' => ['user_agent' => self::USER_AGENT]])
+            ),
             true
         );
+
+        $words = [];
         foreach ($contents as $content) {
             $raw = file_get_contents($content['download_url']);
             foreach (explode("\n", $raw) as $line) {
                 $line = strtoupper(trim($line));
                 if ($line !== '' && ctype_alpha($line) && !in_array($line, self::BLACK_LISTS, true)) {
-                    // ハッシュ関数の決定(参考): https://stackoverflow.com/a/3665527
-                    $this->words[crc32($line)] = $line;
+                    $words[sha1($line, true)] = $line;
                 }
             }
         }
+
+        $this->words = $words;
     }
 
     public function validate(Word $word): bool
     {
-        return isset($this->words[crc32($word->chars)]);
+        return isset($this->words[sha1($word->chars, true)]);
     }
 }
