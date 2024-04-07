88f5a6fd8828d73ee350c05c56eaaba8751fcb8c
ねらひかだ <hkshirorom@yahoo.co.jp>
2024-04-07 15:08:02 +0900

    Update user agent

--- src/Dictionary/WordListDownloader.php
+++ src/Dictionary/WordListDownloader.php
@@ -11,7 +11,7 @@ use stdClass;
 final class WordListDownloader
 {
     public function __construct(
-        private readonly string $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+        private readonly string $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
     ) {
     }
 
